2e0b546f7d694386babd08fe8200623d9ea0d3b0
SaradhaRamesh <saradha2125@gmail.com>
2023-11-08 10:10:57 -0500

    Prime Number

--- src/ProblemSolving/FindingValue.java
+++ src/ProblemSolving/FindingValue.java
@@ -1,5 +1,7 @@
 package ProblemSolving;
 
+import java.util.ArrayList;
+
 public class FindingValue {
 	public void minMax(int arr[]) {
 		int min=arr[0];int max=arr[0];
@@ -27,22 +29,36 @@ public int findValue(int arr[],int n) {
 			}
 	return index;
 }
-public void arrayPrimenumber(int arr[]) {
-	
-	int n= arr.length;
+
+public ArrayList<Integer>primenumber(int arr[] ) {
+	ArrayList<Integer> ar=new  ArrayList<Integer>(); 
 	int count =0;
 	
-	for(int i=0;i<n;i++) {
-		for(int j=2;j<=arr[i];j++)
+	for(int i=0;i<arr.length;i++) {
+		for(int j=2;j<arr[i];j++)
 		{
 			if(arr[i]%j == 0)
 				count++;
 		}
-		if(count == 1)
-		System.out.print(arr[i]+" ");
+		if(count == 0 && arr[i] != 1)
+			ar.add(arr[i]);
 		count =0;
 	}
+	return ar;
 		
+}
+public int primenumber(int a) {
+	int count=0;
+		for(int i=2;i<a;i++) {
+		if(a%i==0) {
+			System.out.print("Not a prime number");
+			count++;
+			break;
+		}
+	}
+	if(count==0&&a!=1)
+		System.out.print("prime number");
+	return 0;
 }
 
 	}
